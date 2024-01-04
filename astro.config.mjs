005507f6153e7db860b4e4998ba4bd347dd89572
ryanbetts <84613835+depatchedmode@users.noreply.github.com>
2024-01-03 21:37:47 -0800

    astro theme customization
    - colors
    - logo
    - opengraph

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,33 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.everywhere.computer',
 	integrations: [
 		starlight({
 			title: 'Everywhere Computer',
+			logo: {
+				light: '/src/assets/logo-light.svg',
+    			dark: './src/assets/logo-dark.svg',
+				replacesTitle: true,
+			},
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],
+			favicon: './src/assets/favicon.svg',
+			head: [
+				{
+					tag: 'meta',
+					attrs: {
+						property: 'og:image',
+						content: './src/assets/open-graph.png',
+					},
+				},
+			],
 			social: {
 				github: 'https://github.com/everywhere-computer',
+				discord: 'https://discord.gg/yU6y8XMGaY',
+				'x.com': 'https://x.com/everywherecomp',
 			},
 			sidebar: [
 				{ label: '👋 Welcome', link: '/' },
